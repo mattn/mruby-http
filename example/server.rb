f23d0eb25693ce401e35d9217e8196e293aa8024
mattn <mattn.jp@gmail.com>
2012-05-08 13:43:50 +0900

    use run_once to run GC.

--- example/server.rb
+++ example/server.rb
@@ -3,6 +3,7 @@ require 'UV'
 
 s = UV::TCP.new()
 s.bind(UV::ip4_addr('127.0.0.1', 8888))
+s.data = []
 s.listen(50) {|x|
   return if x != 0
   c = s.accept()
@@ -11,12 +12,13 @@ s.listen(50) {|x|
     h.parse(b) {|x|
       # TODO: response object
       c.write("HTTP/1.1 200 OK\r\nContent-Length: 6\r\n\r\nhello\n") {|r|
-        c.close {|x|
-          c = nil
-          h = nil
-        }
+        c.close
       }
     }
   }
+  s.data << c
 }
-UV::run()
+while 1 do
+  # NOTE: must be call run_once to run GC.
+  UV::run_once()
+end
