7e53143295e82dcaa438f05a6f89ed95eabe2e39
mattn <mattn.jp@gmail.com>
2012-09-26 17:40:49 +0900

    add example for HTTP::Parser

---
+++ example/http.rb
@@ -0,0 +1,9 @@
+h = HTTP::Parser.new()
+q = "GET http://localhost:8080/foo?bar=baz#zzz HTTP/1.1\r\nHost: localhost\r\nUser-Agent: mruby\r\n\r\n"
+h.parse_request(q) {|h, x|
+  puts x.schema
+  puts x.host
+  puts x.port
+  puts x.path
+  puts x.query
+}
