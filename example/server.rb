2a81a8ada25afbe3e2bd6fc92df840f1978cb586
mattn <mattn.jp@gmail.com>
2012-12-14 18:18:25 +0900

    move http parser as global.

--- example/server.rb
+++ example/server.rb
@@ -1,15 +1,15 @@
 s = UV::TCP.new()
 s.bind(UV::ip4_addr('127.0.0.1', 8888))
-s.data = []
-s.listen(200) {|x|
+h = HTTP::Parser.new()
+s.listen(1024) {|x|
   return if x != 0
   c = s.accept()
   c.read_start {|b|
-    h = HTTP::Parser.new()
     h.parse_request(b) {|r|
       body = "hello #{r.path}"
-      c.write("HTTP/1.1 200 OK\r\nContent-Length: #{body.size}\r\n\r\n#{body}") {|x|
+      c.write("HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: #{body.size}\r\n\r\n#{body}") {|x|
         c.close()
+        c = nil
       }
     }
   }
