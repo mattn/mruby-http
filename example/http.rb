230927f9a06070962ec3ed25ca1b2cba32777a5e
mattn <mattn.jp@gmail.com>
2012-12-19 12:30:12 +0900

    fix example.

--- example/http.rb
+++ example/http.rb
@@ -1,9 +1,36 @@
+#!mruby
+
 h = HTTP::Parser.new()
 q = "GET http://localhost:8080/foo?bar=baz#zzz HTTP/1.1\r\nHost: localhost\r\nUser-Agent: mruby\r\n\r\n"
 h.parse_request(q) {|x|
+  puts x.method
   puts x.schema
   puts x.host
   puts x.port
   puts x.path
   puts x.query
 }
+puts
+
+q = "GET http://localhost/foo?bar=baz#zzz HTTP/1.1\r\nHost: localhost\r\nUser-Agent: mruby\r\n\r\n"
+h.parse_request(q) {|x|
+  puts x.method
+  puts x.schema
+  puts x.host
+  puts x.port
+  puts x.path
+  puts x.query
+}
+puts
+
+q = "POST http://localhost/foo?bar=baz#zzz HTTP/1.1\r\nHost: localhost\r\nUser-Agent: mruby\r\nContent-Length: 5\r\n\r\nhello"
+h.parse_request(q) {|x|
+  puts x.method
+  puts x.schema
+  puts x.host
+  puts x.port
+  puts x.path
+  puts x.query
+  puts x.body
+}
+puts
