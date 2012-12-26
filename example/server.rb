cd957daf2dbccd07f317213c60a54dccbd13e135
mattn <mattn.jp@gmail.com>
2012-12-26 13:05:51 +0900

    keep alive.

--- example/server.rb
+++ example/server.rb
@@ -1,19 +1,24 @@
 h = HTTP::Parser.new()
 s = UV::TCP.new()
 s.bind(UV::ip4_addr('127.0.0.1', 8888))
-s.listen(1024) {|x|
+s.listen(1024) do |x|
   return if x != 0
   c = s.accept()
-  c.read_start {|b|
-    return unless b
-    r = h.parse_request(b)
-    body = "hello #{r.path}"
-    c.write("HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: #{body.size}\r\n\r\n#{body}") {|x|
-      c.close() if c
-      c = nil
-    }
-  }
-}
+  c.read_start do |b|
+    if b
+      r = h.parse_request(b)
+      body = "hello #{r.path}"
+      if !r.headers.has_key?('Connection') || r.headers['Connection'] != 'Keep-Alive'
+        c.write("HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: #{body.size}\r\n\r\n#{body}") do |x|
+          c.close() if c
+          c = nil
+        end
+      else
+        c.write("HTTP/1.1 200 OK\r\nConnection: keep-alive\r\nContent-Length: #{body.size}\r\n\r\n#{body}")
+      end
+    end
+  end
+end
 
 t = UV::Timer.new
 t.start(3000, 3000) {|x|
