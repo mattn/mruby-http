85e153c47a09656a994a9bf24d58f3e198bb84b0
mattn <mattn.jp@gmail.com>
2014-05-03 03:01:33 +0900

    Add file server

---
+++ example/file-server.rb
@@ -0,0 +1,63 @@
+cmap = {
+ 'txt'  => 'text/plain',
+ 'html' => 'text/html',
+ 'css'  => 'text/css',
+ 'jpg'  => 'image/jpeg',
+ 'png'  => 'image/png',
+ 'gif'  => 'image/gif',
+}
+h = HTTP::Parser.new()
+s = UV::TCP.new()
+s.bind(UV::ip4_addr('127.0.0.1', 8888))
+s.listen(1024) do |x|
+  return if x != 0
+  c = s.accept()
+  c.read_start do |b|
+    next unless b
+    r = h.parse_request(b)
+    keep_alive = r.headers.has_key?('Connection') && r.headers['Connection'] == 'Keep-Alive'
+    file = r.path + (r.path[-1] == '/' ? 'index.html' : '')
+	size = -1
+    begin
+      cc = keep_alive ? "keep-alive" : "close"
+      size = UV::FS::stat("public/#{file}").size
+      ext = file.split(".")[-1]
+      ctype = cmap[ext] || 'application/octet-stream'
+      header = "HTTP/1.1 200 OK\r\nConnection: #{cc}\r\nContent-Type: #{ctype}\r\nContent-Length: #{size}\r\n\r\n"
+      f = UV::FS::open("public#{file}", UV::FS::O_RDONLY, UV::FS::S_IREAD)
+      begin
+        nr = 0
+        while nr < size
+          read = f.read(8192, nr)
+          if nr == 0
+            c.write(header + read)
+          else
+            c.write(read)
+          end
+          nr += read.size
+        end
+	  rescue
+      end
+      f.close
+    rescue
+      if size >= 0
+        c.write("HTTP/1.0 500 Internal Server Error\r\n\r\nInternal Server Error") if c
+      else
+        c.write("HTTP/1.0 404 Not Found\r\n\r\nInternal Server Error") if c
+      end if c
+      keep_alive = false
+    end
+    unless c && keep_alive
+      c.close
+      c = nil
+    end
+  end
+end
+
+t = UV::Timer.new
+t.start(3000, 3000) {|x|
+  UV::gc()
+  GC.start
+}
+
+UV::run()
