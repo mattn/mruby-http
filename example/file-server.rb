d6a2bfaae86dc94c02948df6ceaea295f33b1fd6
mattn <mattn.jp@gmail.com>
2014-05-07 14:36:47 +0900

    Fix keep-alive

--- example/file-server.rb
+++ example/file-server.rb
@@ -18,34 +18,35 @@ s.listen(1024) do |x|
     c.data += b
     i = c.data.index("\r\n\r\n")
     next if i == nil || i < 0
-    r = h.parse_request(b)
+    r = h.parse_request(c.data)
+    r.body = c.data.slice(i + 4, c.data.bytesize - i - 4)
     keep_alive = r.headers.has_key?('Connection') && r.headers['Connection'] == 'Keep-Alive'
     path = r.path || '/'
     file = path + (path[-1] == '/' ? 'index.html' : '')
     size = -1
-    nr = 0
+    nw = 0
     begin
       cc = keep_alive ? "keep-alive" : "close"
-      size = UV::FS::stat("public/#{file}").size
+      size = UV::FS::stat("public/#{file}").size.to_i
       ext = file.split(".")[-1]
       ctype = cmap[ext] || 'application/octet-stream'
       header = "HTTP/1.1 200 OK\r\nConnection: #{cc}\r\nContent-Type: #{ctype}\r\nContent-Length: #{size}\r\n\r\n"
       f = UV::FS::open("public#{file}", UV::FS::O_RDONLY, UV::FS::S_IREAD)
       begin
-        while nr < size
-          read = f.read(8192, nr)
-          if nr == 0
+        while nw < size
+          read = f.read(8192, nw)
+          if nw == 0
             c.write(header + read)
           else
             c.write(read)
           end
-          nr += read.size
+          nw += read.bytesize
         end
       rescue
       end
       f.close
     rescue
-      if == 0
+      if nw == 0
         if size >= 0
           c.write("HTTP/1.0 500 Internal Server Error\r\n\r\nInternal Server Error") if c
         else
@@ -54,8 +55,9 @@ s.listen(1024) do |x|
       end
       keep_alive = false
     end
-    unless c && keep_alive
-      c.close
+    c.data = ''
+    unless keep_alive
+      c.close if c
       c = nil
     end
   end
