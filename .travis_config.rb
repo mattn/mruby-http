d231f5da11a0405f19ee3482e155001419618912
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-10-17 13:01:54 +0900

    fix .travis*

--- .travis_config.rb
+++ .travis_config.rb
@@ -1,9 +1,11 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
   enable_debug
+  enable_test
 
   gem :core => 'mruby-print'
   gem :core => 'mruby-sprintf'
   gem :core => 'mruby-time'
   gem "#{MRUBY_ROOT}/.."
 end
+
